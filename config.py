555a6cb19abe051fb40f2684ce79727abb8f15ee
Gingernetics <janstefankowalski@gmail.com>
2018-03-16 02:08:54 -0400

    Student Page

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'This-is-a-secret-key'
 
 
-if os.environ.get('DATABASE_URL') is None:
+#if os.environ.get('DATABASE_URL') is None:
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ARISTAWebDev@localhost/ptc.db'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ptc.db')
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ptc.db')
+#else:
+#    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
